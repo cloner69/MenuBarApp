24ceafd3ef7a568dd8404edb3806a1edf1647950
AdrianSA <50012235+cloner69@users.noreply.github.com>
2025-05-17 03:55:59 +0700

    Menu Bar App

---
+++ MenuBarApp/MenuBarApp/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  MenuBarApp
+//
+//  Created by Adrian Suryo Abiyoga on 02/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("H")
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
